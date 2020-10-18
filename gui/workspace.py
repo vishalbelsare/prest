f9693f23c012b4ce8aef1a9778a5f7c97f1d3358
Matus Tejiscak <ziman@functor.sk>
2020-10-18 15:57:06 +0200

    Add the Hybrid Domination model.

--- gui/workspace.py
+++ gui/workspace.py
@@ -33,8 +33,8 @@ class Workspace:
         self.datasets: List[dataset.Dataset] = []
 
     def save_to_file(self, worker : Worker, fname: str) -> None:
-        with bz2.open(fname, 'wb') as f:
-            f = cast(FileOut, f)  # assert we're doing output
+        with bz2.open(fname, 'wb') as f_bz2:
+            f = cast(FileOut, f_bz2)  # assert we're doing output
 
             f.write(PREST_SIGNATURE)
             intC.encode(f, FILE_FORMAT_VERSION)  # version
@@ -47,8 +47,8 @@ class Workspace:
             listCP(DatasetCP).encode(worker, f, self.datasets)
 
     def load_from_file(self, worker : Worker, fname: str) -> None:
-        with bz2.open(fname, 'rb') as f:
-            f = cast(FileIn, f)  # assert we're doing input
+        with bz2.open(fname, 'rb') as f_bz2:
+            f = cast(FileIn, f_bz2)  # assert we're doing input
 
             sig = f.read(len(PREST_SIGNATURE))
             if sig != PREST_SIGNATURE:
